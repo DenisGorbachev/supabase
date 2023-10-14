44df5b7ede348d893df5a58b1ad1519dc8f04d3d
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-10-14 17:11:55 +0700

    Thinking

--- cache.ts
+++ cache.ts
@@ -12,7 +12,7 @@ export async function getData<T>(path: string): Promise<T | undefined> {
     const data = await sb(bucket.download(`${cacheFolder}/${path}`))
     return JSON.parse(await data.text())
   } catch (error) {
-    if (isSupabaseError(error) && error.message === 'The resource was not found') {
+    if (isSupabaseError(error) && notFoundMessages.includes(error.message)) {
       return undefined
     } else {
       throw error
@@ -33,3 +33,5 @@ const cacheBucketId = process.env.CACHE_BUCKET_ID ?? 'cache'
  * We can't reuse the old cache after we deploy a new version of the project
  */
 const cacheFolder = process.env.VERCEL_GIT_COMMIT_SHA ?? 'local'
+
+const notFoundMessages = ['The resource was not found', 'Object not found']
